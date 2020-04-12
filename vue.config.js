5181a46460c64fd267652e97235da785fb91d1c2
lzzqwe <352840359@qq.com>
2020-04-12 14:06:42 +0800

    dev

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,6 @@ const pxtorem = require('postcss-pxtorem');
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
-
 module.exports = {
     chainWebpack: (config) => {
         config.resolve.alias
@@ -16,15 +15,6 @@ module.exports = {
             .set('base', resolve('src/base'))
     },
     devServer: {
-        // proxy: {
-        //     '/': { // 匹配所有以 '/api'开头的请求路径
-        //         target: 'http://127.0.0.1:3000', // 代理目标的基础路径
-        //         changeOrigin: true, // 支持跨域
-        //         pathRewrite: { // 重写路径: 去掉路径中开头的'/api'
-        //             '^/': ''
-        //         }
-        //     }
-        // },
         open: true,
         host: '0.0.0.0', // 将 host:'localhost' 改成 host:'0.0.0.0',
         port: 8088
